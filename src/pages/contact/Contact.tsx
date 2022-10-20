d9caa607cb6b78ecd8f8bc23760ca90d8cd81745
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-10-20 18:01:37 +0300

    feat: [C4R-819] hide form on submit

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ContactConfig } from './configs/Contact.config';
 import { Controller, useForm } from 'react-hook-form';
@@ -6,16 +6,24 @@ import ContactInputField from '../../common/components/contact-input-field/Conta
 import Textarea from '../../common/components/textarea/Textarea';
 
 const Contact = () => {
+  const [showForm, setShowForm] = useState(true);
   const { t } = useTranslation('contact');
 
   const {
     handleSubmit,
     control,
+    reset,
     formState: { errors },
   } = useForm({
     mode: 'onChange',
     reValidateMode: 'onChange',
   });
+
+  const onSendMail = () => {
+    console.log('Not yet implemented');
+    setShowForm(false);
+    reset();
+  };
 
   return (
     <div className="mx-10 max-w-screen-xl lg:mx-10 xl:mx-auto">
@@ -29,85 +37,89 @@ const Contact = () => {
             {t('description_2')}
           </p>
         </div>
-        <div className="mt-5 lg:mt-12">
-          <form className="flex flex-col mt-5 lg:mt-12">
-            <div className="flex gap-x-6 mb-8">
-              <div className="flex-1">
-                <Controller
-                  key={ContactConfig.name.key}
-                  name={ContactConfig.name.key}
-                  rules={ContactConfig.name.rules}
-                  control={control}
-                  render={({ field: { onChange, value } }) => {
-                    return (
-                      <ContactInputField
-                        config={{
-                          ...ContactConfig.name.config,
-                          name: ContactConfig.name.key,
-                          error: errors[ContactConfig.name?.key]?.message,
-                          defaultValue: value,
-                          onChange: onChange,
-                        }}
-                      />
-                    );
-                  }}
-                />
+        {showForm && (
+          <div className="mt-5 lg:mt-12">
+            <form className="flex flex-col mt-5 lg:mt-12">
+              <div className="flex gap-x-6 mb-8">
+                <div className="flex-1">
+                  <Controller
+                    key={ContactConfig.name.key}
+                    name={ContactConfig.name.key}
+                    rules={ContactConfig.name.rules}
+                    control={control}
+                    render={({ field: { onChange, value } }) => {
+                      return (
+                        <ContactInputField
+                          config={{
+                            ...ContactConfig.name.config,
+                            name: ContactConfig.name.key,
+                            error: errors[ContactConfig.name?.key]?.message,
+                            defaultValue: value,
+                            onChange: onChange,
+                          }}
+                        />
+                      );
+                    }}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="flex gap-x-6 mb-8">
-              <div className="flex-1">
-                <Controller
-                  key={ContactConfig.email.key}
-                  name={ContactConfig.email.key}
-                  rules={ContactConfig.email.rules}
-                  control={control}
-                  render={({ field: { onChange, value } }) => {
-                    return (
-                      <ContactInputField
-                        config={{
-                          ...ContactConfig.email.config,
-                          name: ContactConfig.email.key,
-                          error: errors[ContactConfig.email?.key]?.message,
-                          defaultValue: value,
-                          onChange: onChange,
-                        }}
-                      />
-                    );
-                  }}
-                />
+              <div className="flex gap-x-6 mb-8">
+                <div className="flex-1">
+                  <Controller
+                    key={ContactConfig.email.key}
+                    name={ContactConfig.email.key}
+                    rules={ContactConfig.email.rules}
+                    control={control}
+                    render={({ field: { onChange, value } }) => {
+                      return (
+                        <ContactInputField
+                          config={{
+                            ...ContactConfig.email.config,
+                            name: ContactConfig.email.key,
+                            error: errors[ContactConfig.email?.key]?.message,
+                            defaultValue: value,
+                            onChange: onChange,
+                          }}
+                        />
+                      );
+                    }}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="flex gap-x-6 mb-8">
-              <div className="flex-1">
-                <Controller
-                  key={ContactConfig.message.key}
-                  name={ContactConfig.message.key}
-                  rules={ContactConfig.message.rules}
-                  control={control}
-                  render={({ field: { onChange, value } }) => {
-                    return (
-                      <Textarea
-                        config={{
-                          ...ContactConfig.message.config,
-                          name: ContactConfig.message.key,
-                          error: errors[ContactConfig.message?.key]?.message,
-                          defaultValue: value,
-                          onChange: onChange,
-                        }}
-                      />
-                    );
-                  }}
-                />
+              <div className="flex gap-x-6 mb-8">
+                <div className="flex-1">
+                  <Controller
+                    key={ContactConfig.message.key}
+                    name={ContactConfig.message.key}
+                    rules={ContactConfig.message.rules}
+                    control={control}
+                    render={({ field: { onChange, value } }) => {
+                      return (
+                        <Textarea
+                          config={{
+                            ...ContactConfig.message.config,
+                            name: ContactConfig.message.key,
+                            error: errors[ContactConfig.message?.key]?.message,
+                            defaultValue: value,
+                            onChange: onChange,
+                          }}
+                        />
+                      );
+                    }}
+                  />
+                </div>
               </div>
-            </div>
+            </form>
             <button
+              type="button"
               className="w-3/5 md:w-2/5 yellow-long-button text-center mb-10"
-              onClick={() => console.log('Not yet implemented')}
+              onClick={onSendMail}
             >
               {t('send')}
             </button>
-          </form>
-        </div>
+          </div>
+        )}
+        {!showForm && <div className="mt-5 bg-red-400 lg:mt-12">Hey</div>}
       </div>
     </div>
   );
