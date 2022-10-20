622c661105c8a874f84d86c2be7ee2e84bce5585
Andreea Pascu <andreeamaria10p@gmail.com>
2022-10-20 15:59:14 +0300

    feat: [C4R-823] added responsiveness

--- src/pages/landing/components/counter/Counter.tsx
+++ src/pages/landing/components/counter/Counter.tsx
@@ -12,7 +12,7 @@ interface CounterProps {
 
 const Counter = ({ counterItems }: CounterProps) => {
   return (
-    <div className="bg-yellow w-full h-56 flex flex-row items-center justify-center gap-x-48">
+    <div className="bg-yellow w-full sm:h-56 h-72 flex sm:flex-row flex-col items-center justify-center lg:gap-x-48 gap-x-28 gap-y-10 px-16">
       {counterItems.map((counterItem, index) => (
         <CounterItem value={counterItem.value} title={counterItem.title} key={index}></CounterItem>
       ))}
--- src/pages/landing/components/counter/CounterItem.tsx
+++ src/pages/landing/components/counter/CounterItem.tsx
@@ -4,7 +4,7 @@ import { ICounterItem } from './Counter';
 const CounterItem = ({ value, title }: ICounterItem) => {
   return (
     <div className="flex flex-col items-center max-w-xs text-center gap-y-4">
-      <p className="font-titilliumSemiBold sm:text-2xl lg:text-7xl text-lg">{value}</p>
+      <p className="font-titilliumSemiBold sm:text-6xl lg:text-7xl text-4xl">{value}</p>
       <p className="sm:text-xl text-sm">{title}</p>
     </div>
   );
