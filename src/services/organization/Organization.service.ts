3c743ec3f28d797f46b7a020cc1f9779b93dff46
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2022-11-02 17:34:24 +0200

    refactor(ngo-search): use axios params for filters

--- src/services/organization/Organization.service.ts
+++ src/services/organization/Organization.service.ts
@@ -1,4 +1,5 @@
 import { ISelectData } from '../../common/helpers/Nomenclature.helper';
+import { OrganizationFlat } from '../../common/interfaces/OrganizationFlat.interface';
 import { PaginatedEntity } from '../../common/interfaces/PaginatedEntity.interface';
 import API from '../API';
 
@@ -8,14 +9,14 @@ export const searchOrganizations = async (
   search?: string,
   locationId?: ISelectData,
   domains?: ISelectData[],
-): Promise<PaginatedEntity<any>> => {
-  let requestUrl = `organization/practice-program?limit=${limit}&page=${page}`;
-
-  if (search) requestUrl = `${requestUrl}&search=${search}`;
-
-  if (locationId) requestUrl = `${requestUrl}&locationId=${locationId.value}`;
-
-  if (domains) requestUrl = `${requestUrl}&${domains.map((f) => `domains[]=${f.value}`).join('&')}`;
-
-  return API.get(requestUrl).then((res) => res.data);
+): Promise<PaginatedEntity<OrganizationFlat>> => {
+  return API.get('organization/practice-program', {
+    params: {
+      limit,
+      page,
+      search: search || undefined,
+      locationId: locationId?.value,
+      domains: domains?.map((domain) => domain.value),
+    },
+  }).then((res) => res.data);
 };
