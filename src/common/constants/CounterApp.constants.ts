a4e7a6cbc9a7f48652637f547b10731d2bbcdbfe
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-11-07 18:52:16 +0200

    feat: [C4R-937] added fe counter

---
+++ src/common/constants/CounterApp.constants.ts
@@ -0,0 +1,4 @@
+export enum CounterApp {
+  PRACTICE_PROGRAM = 'practice-program',
+  CIVIC_SERVICE = 'civic-service',
+}
--- src/pages/landing/components/counter/CounterItem.tsx
+++ src/pages/landing/components/counter/CounterItem.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import i18n from '../../../../common/configs/i18n';
+import { useGetLandingCountersQuery } from '../../../../services/statistics/Statistics.queries';
 import { ICounterItem } from './Counter';
+import { CounterApp } from '../../../../common/constants/CounterApp.constants';
 
 const CounterItem = ({ value, type }: ICounterItem) => {
+  // const { landingCounters } = useStatistics();
+  const { data: landingCounters, error } = useGetLandingCountersQuery(CounterApp.CIVIC_SERVICE);
   const title = i18n.t(`landing:counter:${type}`);
   return (
     <div className="flex flex-col items-center max-w-xs text-center gap-y-4">
-      <p className="font-titilliumSemiBold sm:text-6xl lg:text-7xl text-4xl">{value}</p>
+      <p className="font-titilliumSemiBold sm:text-6xl lg:text-7xl text-4xl">
+        {landingCounters?.activeItems}
+      </p>
       <p className="body-text">{title}</p>
     </div>
   );
