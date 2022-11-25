ea7ea23b3d94d8073e5d3a5d0feb8e7233ea137d
Dragos-Paul Strat <dragos1195@gmail.com>
2022-11-25 17:48:35 +0200

    fix: [C4R-975] fix virtuoso -> virtuoso grid + ngo card adjustments

--- src/pages/organizations/components/OrganizationItem.tsx
+++ src/pages/organizations/components/OrganizationItem.tsx
@@ -9,31 +9,29 @@ const OrganizationItem = ({ organization }: { organization: OrganizationFlat }) 
   const navigate = useNavigate();
 
   return (
-    <div className="mt-10">
-      <Card>
-        <div className="flex flex-col sm:gap-y-10 gap-y-5 h-full">
-          <div className="aspect-square lg:w-32 sm:w-24 w-full lg:min-w-[8rem] sm:min-w-[6rem] bg-gray-100 sm:max-h-full max-h-[8rem]">
-            {organization.logo && (
-              <img
-                className="bg-cover h-full w-full"
-                alt="organization image"
-                src={organization.logo}
-              ></img>
-            )}
-          </div>
-          <div className="flex flex-col gap-y-5">
-            <p className="subtitle">{organization.name}</p>
-            <p className="article card-text-overflow">{organization.description}</p>
-          </div>
-          <button
-            className="yellow-button w-full mt-auto"
-            onClick={() => navigate(`${organization.id}`)}
-          >
-            {t('action')}
-          </button>
+    <Card>
+      <div className="flex flex-col sm:gap-y-10 gap-y-5 h-full">
+        <div className="aspect-square lg:w-32 sm:w-24 w-full lg:min-w-[8rem] sm:min-w-[6rem] bg-gray-100 sm:max-h-full max-h-[8rem]">
+          {organization.logo && (
+            <img
+              className="bg-cover h-full w-full"
+              alt="organization image"
+              src={organization.logo}
+            ></img>
+          )}
         </div>
-      </Card>
-    </div>
+        <div className="flex flex-col gap-y-5">
+          <p className="subtitle">{organization.name}</p>
+          <p className="article card-text-overflow">{organization.description}</p>
+        </div>
+        <button
+          className="yellow-button w-full mt-auto"
+          onClick={() => navigate(`${organization.id}`)}
+        >
+          {t('action')}
+        </button>
+      </div>
+    </Card>
   );
 };
 
